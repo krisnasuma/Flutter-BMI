440b07dd8d0eaa3fbe970559ab194b9794786ebb
krisnasuma <45704616+krisnasuma@users.noreply.github.com>
2020-04-04 22:05:20 +0800

    Create about_me.dart

---
+++ about_me.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyProfil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      new Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("ABOUT ME"),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            image: DecorationImage(image: AssetImage('images/krisna.JPEG'),
+                                fit: BoxFit.cover
+                            )
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              color: Colors.white,
+              child: Column(
+
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Text('Gede Krisna Kesuma Wijaya',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox(height: 5,child: Container(
+                    color: Colors.lightBlue,
+                  ),),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('krisna.kesuma@undiksha.ac.id',
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black.withOpacity(0.6)
+                        ),
+                      )
+
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30, right: 50, left: 50),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueGrey),
+                            //color: Colors.green,
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.my_location,
+                                    color: Colors.purple,
+                                    size: 58),
+                                Text('Singaraja',
+                                  style: TextStyle
+                                    (color: Colors.deepOrange,
+                                      fontWeight: FontWeight.bold
+                                  ), )
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            //color: Colors.green,
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.home, color: Colors.green, size: 58,),
+                                Text('Buleleng', style: TextStyle
+                                  (color: Colors.yellow,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            //color: Colors.green ,
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+
+
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.music_note,color: Colors.orangeAccent, size: 58,),
+                                Text('Pop', style: TextStyle
+                                  (color: Colors.greenAccent,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                          ),
+
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: <Widget>[
+                                Icon(Icons.school, color: Colors.lightBlueAccent , size: 58,),
+                                Text('Undiksha',style: TextStyle
+                                  (color: Colors.pink,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+  }
+}
